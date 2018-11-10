2e35b05aab338c4c94b61db2771abeee7815224d
Vlad Dinu <vlad.dinu@efden.org>
2018-11-10 23:31:53 +0000

    Refactor project structure

--- utils/header.py
+++ utils/header.py
@@ -11,6 +11,30 @@ boilerplate_header = {
 }
 
 def generate_branded_header(request_type, brand, face_value):
+    data = {
+        "client_request_id": str(signature.client_request_id),
+        "brand": "tesco",
+        "face_value": {
+            "amount": 10,
+            "currency": "GBP"
+        },
+        "delivery_method": "code",
+        "fulfilment_by": "rewardcloud",
+        "fulfilment_parameters": {
+            "to_name": "Receiver",
+            "to_email": "vlad@reward.cloud",
+            "from_name": "Reward Cloud",
+            "from_email": "noreply@reward.cloud",
+            "subject": "[TestCode] Here is your gift card!"
+        },
+        "personalisation": {
+            "to_name": "Recipient",
+            "from_name": "Sender",
+            "message": "Here is your gift",
+            "template": "standard"
+        },
+        "sector": "voluntary-benefits"
+    }
     header = boilerplate_header
     header["Signature"] = signature.generate_signature(request_type, brand, face_value)
     header["Timestamp"] = signature.timestamp
